b5cea9faefa3594cd58a9d9a79e08d78115f14cc
Ryan Miller <rmiller@compute-105.cm.cluster>
2023-05-19 15:52:58 -0400

    updated path in permissions script

--- _jhpce_org/update_permissions.sh
+++ _jhpce_org/update_permissions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh /dcs04/lieber/lcolladotor/_jhpce_org_LIBD001/update_permissions_spatialteam.sh /dcs04/lieber/marmaypag/spatialHYP_LIBD4195/spatial_HYP
+sh /dcs04/lieber/lcolladotor/_jhpce_org_LIBD001/update_permissions_spatialteam.sh /dcs04/lieber/marmaypag/spatialHYP_LIBD4195/spatial_HYP/
