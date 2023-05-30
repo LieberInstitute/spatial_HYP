4e3db4db061892b4befacb704fd47332daa9de5e
Ryan Miller <rmiller@compute-095.cm.cluster>
2023-05-30 11:17:40 -0400

    updated permissions scripts for project

--- _jhpce_org/update_permissions.sh
+++ _jhpce_org/update_permissions.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+#$ -cwd
+#$ -l mem_free=2G,h_vmem=2G,h_fsize=400G
+#$ -N update_permissions
+#$ -o logs/update_permissions.txt
+#$ -e logs/update_permissions.txt
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
 
 sh /dcs04/lieber/lcolladotor/_jhpce_org_LIBD001/update_permissions_spatialteam.sh /dcs04/lieber/marmaypag/spatialHYP_LIBD4195/spatial_HYP/
