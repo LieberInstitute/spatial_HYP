79f2191325f85c48f50ca0878505cfe3a6cbd9bb
Bernie Mulvey <bmulvey1@jh.edu>
2024-06-07 18:27:01 -0400

    piecewise commit the restructure

--- raw-data/FASTQ/raw-mkdir.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --array=1
-
-SAMPLE=$(awk "NR==${SLURM_ARRAY_TASK_ID}" raw-mkdir.txt)
-
-mkdir -p ./logs/
-mkdir -p /dcs04/lieber/marmaypag/spatialHYP_LIBD4195/spatial_HYP/raw-data/FASTQ/${SAMPLE}/
-
-mv slurm-*.out ./logs
