f2c352704b3b914a0fa3755c0da673a534a99e2e
Bernie Mulvey <berniejmulvey@gmail.com>
2025-11-22 19:39:03 -0600

    fix ldsc result concat script

--- spatial_HYP/code/16-sLDSC-SEG_vARCvVMH_directionalSexDE/ldsc_results2.R
+++ spatial_HYP/code/16-sLDSC-SEG_vARCvVMH_directionalSexDE/ldsc_results2.R
@@ -1,71 +1,72 @@
 
 library(data.table)
 # read data 
-dat <- read.table("ldsc_results.txt",as.is=T,header=T,sep="\t")
+setwd("/dcs04/lieber/marmaypag/spatialHYP_LIBD4195/spatial_HYP/spatial_HYP/code/16-sLDSC-SEG_vARCvVMH_directionalSexDE")
+dat <- fread("ldsc_results.txt",as.is=T,header=T,sep="\t")
 
 traits <- c(
 "ADHD",
+"Drinks Per Week",
+"Alzheimer Disease",
+"Alzheimer Disease2",
 "Alzheimer Disease3",
- "Anorexia",
- "Autism",
- "BMI",
- "Bipolar Disorder2",
- "GSCAN_AgeSmk",
- "GSCAN_CigDay",
- "GSCAN_DrnkWk",
- "GSCAN_SmkCes",
- "GSCAN_SmkInit",
- "Height",
- "Type_2_Diabetes",
- "mdd2019edinburgh",
- "epilepsy",
- "Intelligence",
- "Parkinson Disease",
- "Schizophrenia_PGC3",
- "Education Years",
- "Neuroticism"
+"Anorexia",
+"AUD_NatMed2023",
+"Autism",
+"BMI",
+"Bipolar Disorder2",
+"Bipolar Disorder",
+"Alcohol Use Disorder",
+"epilepsyAll",
+"epilepsyFocal",
+"epilepsy",
+"GSCAN_AgeSmk",
+"GSCAN_CigDay",
+"GSCAN_DrnkWk",
+"GSCAN_SmkCes",
+"GSCAN_SmkInit",
+"Height",
+"Insomnia",
+"Intelligence",
+"mdd2019edinburgh",
+"Depression_ex23andMe",
+"Depression",
+"STROKE1",
+"STROKE2",
+"OUD_MVP1",
+"OUD_MVP12",
+"OUD_MVP2",
+"Type_2_Diabetes",
+"Parkinson Disease",
+"PTSD",
+"Schizophrenia",
+"Schizophrenia_PGC3",
+"Cigarettes Per Day",
+"STROKEall",
+"Education Years",
+"Neuroticism"
 )
-idx <- is.element(dat$trait,traits)
-dat <- dat[idx,]
+
+dat <- dat[trait %in% traits]
 
 # rename traits
-dat$trait[dat$trait=="GSCAN_AgeSmk"] <- "Age of smoking"
-dat$trait[dat$trait=="GSCAN_CigDay"] <- "Cigarettes per day"
-dat$trait[dat$trait=="GSCAN_DrnkWk"] <- "Drinks per week"
-dat$trait[dat$trait=="GSCAN_SmkCes"] <- "Smoking cessation"
-dat$trait[dat$trait=="GSCAN_SmkInit"] <- "Smoking initiation"
-dat$trait[dat$trait=="Schizophrenia_PGC3"] <- "Schizophrenia"
-dat$trait[dat$trait=="Bipolar Disorder2"] <- "Bipolar"
-dat$trait[dat$trait=="epilepsy"] <- "Epilepsy"
-dat$trait[dat$trait=="Type_2_Diabetes"] <- "Type 2 Diabetes"
-dat$trait[dat$trait=="Alzheimer Disease3"] <- "Alzheimer Disease"
-dat$trait[dat$trait=="mdd2019edinburgh"] <- "Depression"
+dat$trait[trait=="GSCAN_AgeSmk"] <- "Age of smoking"
+dat$trait[trait=="GSCAN_CigDay"] <- "Cigarettes per day"
+dat$trait[trait=="GSCAN_DrnkWk"] <- "Drinks per week"
+dat$trait[trait=="GSCAN_SmkCes"] <- "Smoking cessation"
+dat$trait[trait=="GSCAN_SmkInit"] <- "Smoking initiation"
+dat$trait[trait=="Schizophrenia_PGC3"] <- "Schizophrenia (PGC3)"
+dat$trait[trait=="Bipolar Disorder2"] <- "Bipolar (PGC2)"
+dat$trait[trait=="epilepsy"] <- "Epilepsy"
+dat$trait[trait=="Type_2_Diabetes"] <- "Type 2 Diabetes"
+dat$trait[trait=="Alzheimer Disease3"] <- "Alzheimer Disease v3"
+dat$trait[trait=="mdd2019edinburgh"] <- "Depression"
 
 tmpdat <- copy(as.data.table(dat))
 
-# FDR correction -- sex de and marker tests jointly
+# FDR correction
 dat$p_zcore <- pnorm(abs(dat$Coefficient_z.score),lower.tail=F)*2
 dat$FDR <- p.adjust(dat$p_zcore,method="fdr")
 
 
-write.csv(dat,"ldsc_results_MkAndSexFDRcorrection.csv")
-
-# FDR correction by gene set type
-datmk <- copy(tmpdat)[cell %in% grep(cell,pattern="mk",value=T)]
-datmk[,p_zcore:=pnorm(abs(Coefficient_z.score),lower.tail=F)*2]
-datmk[,FDR:=p.adjust(p_zcore,method="fdr")]
-
-
-write.csv(datmk,"ldsc_results_MkSetsFDR.csv")
-
-
-datsex <- copy(tmpdat)[cell %in% grep(cell,pattern="sexDE",value=T)]
-datsex[,p_zcore:=pnorm(abs(Coefficient_z.score),lower.tail=F)*2]
-datsex[,FDR:=p.adjust(p_zcore,method="fdr")]
-
-
-write.csv(datsex,"ldsc_results_SexDEsetsFDR.csv")
-
-
-
-
+write.csv(dat,"ldsc_results_directional_SexDEsetsFDR_vVMH_vARC_only.csv")
